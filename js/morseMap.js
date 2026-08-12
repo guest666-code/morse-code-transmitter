65c4008dbb96a87d28305f5cac1d13d6c4140cc6
guest666-code <efemertdede9@gmail.com>
2026-08-12 22:24:55 +0300

    Update morseMap.js

--- js/morseMap.js
+++ js/morseMap.js
@@ -9,3 +9,16 @@ export const morseCodeMap = {
     '0': '-----', ' ': '/'
 };
 
+export const reverseMorseMap = Object.fromEntries(
+    Object.entries(morseCodeMap).map(([key, value]) => [value, key])
+);
+
+export function textToMorse(text) {
+    return text.toUpperCase().split('').map(char => morseCodeMap[char] || char).join(' ');
+}
+
+export function morseToText(morse) {
+    return morse.trim().split(' / ').map(word => {
+        return word.split(' ').map(code => reverseMorseMap[code] || code).join('');
+    }).join(' ');
+}
